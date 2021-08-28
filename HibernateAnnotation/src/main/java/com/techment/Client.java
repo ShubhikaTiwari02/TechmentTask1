53010cc757ee106ba6af7acc1017c8ebda3895df
Shubhika <tiwarishubhika02@gmail.com>
2021-08-28 11:55:17 +0530

    assignmentdata

---
+++ HibernateAnnotation/src/main/java/com/techment/Client.java
@@ -0,0 +1,27 @@
+package com.techment;
+ import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class Client {
+
+	public static void main(String[] args) {
+		
+		Configuration configuration = new Configuration();
+		configuration.configure("hibernate.cfg.xml");
+		SessionFactory sessionFactory = configuration.buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		Transaction tx= session.beginTransaction();
+		Student student= new Student();
+		student.setStudentId(2);
+		student.setName("shubhika");
+		student.setDept("MCA");
+		session.persist(student);
+		System.out.println("Inserted");
+		tx.commit();
+		session.close();
+
+	}
+
+}
